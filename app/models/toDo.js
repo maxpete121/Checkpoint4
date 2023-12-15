2e56be21f7528ef559fd9f0f303d256e9a099b2f
max <maxpete121@gmail.com>
2023-12-15 10:08:55 -0700

    first

---
+++ app/models/toDo.js
@@ -0,0 +1,10 @@
+
+
+
+export class ToDoList{
+    constructor(data){
+        this.completed = data.completed
+        this.description = data.description
+        this.creatorId = data.creatorId
+    }
+}
